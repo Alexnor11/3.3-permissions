1c1932d0b9223eec2f93700424025139abe02d15
Alexnor11 <advance114@gmail.com>
2022-01-31 13:36:51 +0300

    new status

--- advertisements/serializers.py
+++ advertisements/serializers.py
@@ -23,8 +23,8 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Advertisement
-        fields = ('id', 'title', 'description', 'creator',
-                  'status', 'created_at',)
+        fields = ('id', 'title', 'description', 'creator', 'status', 'created_at',)
+        read_only_fields = ['creator', ]
 
     def create(self, validated_data):
         """Метод для создания"""
@@ -38,19 +38,13 @@ class AdvertisementSerializer(serializers.ModelSerializer):
         validated_data["creator"] = self.context["request"].user
         return super().create(validated_data)
 
-    def update(self, instance, validated_data):
-        """Метод для изменения статуса"""
-
-        return super().update(instance, validated_data)
-
     def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
 
         # TODO: добавьте требуемую валидацию
         if data.get('status') == 'OPEN':
-            if Advertisement.objects.filter(status='OPEN',
-                                            creator=self.context['request'].user).count() > 2:
+            if Advertisement.objects.filter(status='OPEN').count() > 2:
+                # creator=self.context['request'].user).count() > 2:
                 raise ValidationError('У вас больше 10 открытых объявлений')
 
         return data
-
--- api_with_restrictions/settings.py
+++ api_with_restrictions/settings.py
@@ -73,20 +73,6 @@ TEMPLATES = [
     },
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'user': '10/minute',
-        'anon': '10/minute',
-    },
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend',
-    ],
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    # 'PAGE_SIZE': 3
-}
 
 WSGI_APPLICATION = 'api_with_restrictions.wsgi.application'
 
@@ -138,8 +124,24 @@ USE_L10N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '10/minute',
+        'anon': '10/minute',
+    },
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 4
+}
